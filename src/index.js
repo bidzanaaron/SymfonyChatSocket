e2ee2246832b987ba4e2aae32db7c8166594c13f
abidzan <abidzan@databay.de>
2024-01-22 16:16:39 +0100

    add authentication of user on connect and safety when sending messages

--- src/index.js
+++ src/index.js
@@ -8,26 +8,52 @@ const io = new Server(3000, {
 
 let connectedClients = [];
 
+function isParticipantInChat(chatId, availableChats) {
+    console.log("Checking availableChats list:", availableChats);
+    for (let i = 0; i < availableChats.length; i++) {
+        if (availableChats[i] !== chatId) { continue; }
+
+        console.log(`${chatId} was found in the list.`);
+        return true;
+    }
+
+    console.log(`${chatId} was not found in the list.`);
+    return false;
+}
+
 io.on("connection", (socket) => {
-    connectedClients.push(socket);
-    
+    socket.on("userInformation", (data) => {
+        const username = data.username;
+        const availableChats = data.availableChats;
+
+        connectedClients.push({
+            socket: socket,
+            username: username,
+            availableChats: availableChats,
+        });
+
+        console.log("");
+        console.log(`User identified as ${username} has connected.`);
+        console.log(`Their available chats are:`);
+        for (let i = 0; i < availableChats.length; i++) {
+            console.log(availableChats[i]);
+        }
+        console.log("");
+    })
+
     socket.on("sendMessage", (data) => {
-        for (let i = 0; i < connectedClients.length; i++) {
-            if (connectedClients[i] === socket) { continue; }
+        let chatParticipants = connectedClients.filter((element) => isParticipantInChat(data.chatId, element.availableChats))
+
+        for (let i = 0; i < chatParticipants.length; i++) {
+            if (chatParticipants[i].socket === socket) { continue; }
             
-            const iteratedSocket = connectedClients[i];
+            const iteratedSocket = chatParticipants[i].socket;
             iteratedSocket.emit("sendMessage", data);
         }
     });
 
     socket.on("disconnect", () => {
-        for (let i = 0; i < connectedClients.length; i++) {
-            if (connectedClients[i] !== socket) { continue; }
-
-            const index = connectedClients.indexOf(connectedClients[i]);
-            if (index > -1) {
-                connectedClients.splice(index, 1);
-            }
-        }
+        let newConnectedClients = connectedClients.filter((element) => element.socket !== socket);
+        connectedClients = newConnectedClients;
     });
 });
